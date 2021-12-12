4a337b36ce62c97badd9ce24e86f14c78627dc18
Adrianxho1020 <aburbanoc@unicauca.edu.co>
2021-12-12 14:13:41 -0500

    Provider and access details

---
+++ provider.tf
@@ -0,0 +1,8 @@
+# Especifica el proveedor y los detalles de acceso
+provider "aws" {
+  region     = "us-east-2"
+  access_key = ""             #Access Key personal
+  secret_key = ""             #Access Key secret personal en AWS
+}
+
+
